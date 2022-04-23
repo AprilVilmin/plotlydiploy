3ea5a3d7d6202b28bd8bd41c75631e233ca0718c
AprilVilmin <aprilnygaard@gmail.com>
2022-04-23 15:17:47 -0500

    update

--- charts.js
+++ charts.js
@@ -17,6 +17,7 @@ function init() {
     var firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildCharts2(firstSample);
+    buildCharts3(firstSample);   
     buildMetadata(firstSample);
   });
 }
@@ -29,6 +30,7 @@ function optionChanged(newSample) {
   buildMetadata(newSample);
   buildCharts(newSample);
   buildCharts2(newSample);
+  buildCharts3(newSample);
   
 }
 
@@ -114,13 +116,14 @@ function buildCharts2(sample) {
     
     var samples = data.samples;
     
-    // 4. Create a variable that filters the samples for the object with the desired sample number.
+    // Create a variable that filters the samples for the object with the desired sample number.
     var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
 
-    //  5. Create a variable that holds the first sample in the array.
+    // Create a variable that holds the first sample in the array.
     var result = resultArray[0];
     console.log(result)
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+
+    // Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otu_ids = result.otu_ids;
     var otu_labels = result.otu_labels;
     var sample_values = result.sample_values;
@@ -158,5 +161,78 @@ function buildCharts2(sample) {
     
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot('bubble', data2, bubbleLayout);
+  });
+}
+
+// Create the buildChart function.
+function buildCharts3(sample) {
+  // Use d3.json to load the samples.json file 
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+
+    // Create a variable that holds the samples array. 
+    var samples = data.samples;
+
+    // Create a variable that filters the samples for the object with the desired sample number.
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadataArray = data.metadata.filter(sampleObj => sampleObj.id == sample);
+
+    // Create a variable that holds the first sample in the array.
+    var result = resultArray[0];
+
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var metadata = metadataArray[0];
+
+    // Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+
+    // 3. Create a variable that holds the washing frequency.
+    var frequency =  parseFloat(metadata.wfreq);
+    // Create the yticks for the bar chart.
+
+    // Use Plotly to plot the bar data and layout.
+    // Plotly.newPlot();
+    
+    // Use Plotly to plot the bubble data and layout.
+    // Plotly.newPlot();
+   
+    
+    // 4. Create the trace for the gauge chart.
+    https://blog.hubspot.com/website/how-to-bold-in-html#:~:text=To%20bold%20the%20text%20in,property%20set%20to%20%E2%80%9Cbold.%E2%80%9D
+    var gaugeData = [
+      {
+        value: frequency,
+        type: "indicator",
+        mode: "gauge+number",
+        title: { text: "<b> Belly Button Washing Frequency </b> <br> Scrubs per Week"},
+        gauge: {
+          axis: { range: [null, 10], tickwidth: 1, tickcolor: "black" },
+          bar: { color: "black" },
+          steps: [
+            { range: [0, 2], color: "red" },
+            { range: [2, 4], color: "orange" },
+            { range: [4, 6], color: "yellow" },
+            { range: [6, 8], color: "limegreen" },
+            { range: [8, 10], color:"green" }
+          ],
+        }
+      }
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+     //title: 'Belly Button Washing Frequency',
+      margin: { t: 0, b: 0 }, 
+      width: 500,
+      height: 500,
+      font: { color: "black", family: "Arial" }
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
